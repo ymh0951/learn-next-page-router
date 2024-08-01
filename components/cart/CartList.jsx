e8917b884d6365a0abf54b1906be79884222190a
Minho Yoo <ymh0951@naver.com>
2024-08-01 20:56:59 +0900

    Next

--- components/cart/CartList.jsx
+++ components/cart/CartList.jsx
@@ -3,6 +3,12 @@ import Image from 'next/image'
 import styles from './CartList.module.css'
 
 export default function CartList({ carts }) {
+  const totalPrice = carts.reduce((acc, cur) => {
+    return acc + cur.price;
+  }, 0);
+
+  console.log(totalPrice);
+
   return (
     <div>
       <div>
--- next.config.mjs
+++ next.config.mjs
@@ -14,3 +14,4 @@ const nextConfig = {
 };
 
 export default nextConfig;
+ 
